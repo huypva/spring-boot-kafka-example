45435745dcb55a840c914d1c526567761573fa97
huypva <huypva@vng.com.vn>
2021-09-09 09:51:15 +0700

    Change annotation

--- spring-boot-kafka/src/main/java/io/codebyexample/springbootkafka/entrypoint/http/HttpController.java
+++ spring-boot-kafka/src/main/java/io/codebyexample/springbootkafka/entrypoint/http/HttpController.java
@@ -5,6 +5,7 @@ import io.codebyexample.springbootkafka.core.usecase.example.UserUseCase;
 import io.codebyexample.springbootkafka.utils.GsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +20,7 @@ public class HttpController {
   @Autowired
   private UserUseCase userUseCase;
 
-  @RequestMapping(value = "/greet/{userId}", method = RequestMethod.POST)
+  @PostMapping(value = "/greet/{userId}")
   public void greet(@PathVariable("userId") long userId,
                     @RequestBody Message message) {
     UserMessage user = UserMessage.builder()
